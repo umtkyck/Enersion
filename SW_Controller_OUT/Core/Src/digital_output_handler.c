52fea8f410386f8f6bdd366ecf81b96c14974f0e
JackieWang04 <jackie.wang@enersion.com>
2025-11-17 12:55:09 -0500

    Fix digital output pin mapping to match main.h configuration
    Updated outputPinMap array in digital_output_handler.c to match exact
    pin definitions from main.h (MCU_DO0 through MCU_DO55).
    
    Key Changes:
    - DO0-DO10: Port F (PF0-PF10)
    - DO11-DO14: Port C (PC0-PC3)
    - DO15-DO22: Port A (PA0-PA7)
    - DO23-DO24: Port C (PC4-PC5)
    - DO25-DO27: Port B (PB0-PB2)
    - DO28-DO29: Port F (PF11-PF12)
    - DO30: Port E (PE15)
    - DO31-DO34: Port B (PB10-PB13)
    - DO35-DO42: Port D (PD8-PD15)
    - DO43-DO49: Port G (PG2-PG8)
    - DO50-DO53: Port C (PC6-PC9)
    - DO54-DO55: Port A (PA8-PA9)
    
    Critical Fixes:
    - DO49: Corrected from PD1 to PG8
    - DO50: Corrected from PD2 to PC6
    - Removed incorrect PD1/PD2 mapping (reserved for LEDs)
    - Pin order now matches hardware schematic exactly
    
    This ensures digital outputs control the correct physical pins.

--- SW_Controller_OUT/Core/Src/digital_output_handler.c
+++ SW_Controller_OUT/Core/Src/digital_output_handler.c
@@ -13,36 +13,90 @@
 static DigitalOutput_t digitalOutputs[NUM_DIGITAL_OUTPUTS];
 static uint8_t outputStates[NUM_DIGITAL_OUTPUTS];
 
-/* Output pin mapping based on main.c GPIO configuration */
+/* Output pin mapping - MUST match main.h MCU_DO0-DO55 definitions exactly */
 static const struct {
     GPIO_TypeDef* port;
     uint16_t pin;
 } outputPinMap[] = {
-    /* Port F outputs */
-    {GPIOF, GPIO_PIN_0}, {GPIOF, GPIO_PIN_1}, {GPIOF, GPIO_PIN_2}, {GPIOF, GPIO_PIN_3},
-    {GPIOF, GPIO_PIN_4}, {GPIOF, GPIO_PIN_5}, {GPIOF, GPIO_PIN_6}, {GPIOF, GPIO_PIN_7},
-    {GPIOF, GPIO_PIN_8}, {GPIOF, GPIO_PIN_9}, {GPIOF, GPIO_PIN_10}, {GPIOF, GPIO_PIN_11},
-    {GPIOF, GPIO_PIN_12},
-    /* Port C outputs */
-    {GPIOC, GPIO_PIN_0}, {GPIOC, GPIO_PIN_1}, {GPIOC, GPIO_PIN_2}, {GPIOC, GPIO_PIN_3},
-    {GPIOC, GPIO_PIN_4}, {GPIOC, GPIO_PIN_5}, {GPIOC, GPIO_PIN_6}, {GPIOC, GPIO_PIN_7},
-    {GPIOC, GPIO_PIN_8}, {GPIOC, GPIO_PIN_9},
-    /* Port A outputs */
-    {GPIOA, GPIO_PIN_0}, {GPIOA, GPIO_PIN_1}, {GPIOA, GPIO_PIN_2}, {GPIOA, GPIO_PIN_3},
-    {GPIOA, GPIO_PIN_4}, {GPIOA, GPIO_PIN_5}, {GPIOA, GPIO_PIN_6}, {GPIOA, GPIO_PIN_7},
-    {GPIOA, GPIO_PIN_8}, {GPIOA, GPIO_PIN_9},
-    /* Port B outputs */
-    {GPIOB, GPIO_PIN_0}, {GPIOB, GPIO_PIN_1}, {GPIOB, GPIO_PIN_2}, {GPIOB, GPIO_PIN_10},
-    {GPIOB, GPIO_PIN_11}, {GPIOB, GPIO_PIN_12}, {GPIOB, GPIO_PIN_13},
-    /* Port E outputs */
-    {GPIOE, GPIO_PIN_15},
-    /* Port D outputs */
-    {GPIOD, GPIO_PIN_8}, {GPIOD, GPIO_PIN_9}, {GPIOD, GPIO_PIN_10}, {GPIOD, GPIO_PIN_11},
-    {GPIOD, GPIO_PIN_12}, {GPIOD, GPIO_PIN_13}, {GPIOD, GPIO_PIN_14}, {GPIOD, GPIO_PIN_15},
-    {GPIOD, GPIO_PIN_1}, {GPIOD, GPIO_PIN_2},
-    /* Port G outputs */
-    {GPIOG, GPIO_PIN_2}, {GPIOG, GPIO_PIN_3}, {GPIOG, GPIO_PIN_4}, {GPIOG, GPIO_PIN_5},
-    {GPIOG, GPIO_PIN_6}, {GPIOG, GPIO_PIN_7}, {GPIOG, GPIO_PIN_8}
+    /* DO0-DO10: Port F (PF0-PF10) */
+    {GPIOF, GPIO_PIN_0},  // DO0  = PF0
+    {GPIOF, GPIO_PIN_1},  // DO1  = PF1
+    {GPIOF, GPIO_PIN_2},  // DO2  = PF2
+    {GPIOF, GPIO_PIN_3},  // DO3  = PF3
+    {GPIOF, GPIO_PIN_4},  // DO4  = PF4
+    {GPIOF, GPIO_PIN_5},  // DO5  = PF5
+    {GPIOF, GPIO_PIN_6},  // DO6  = PF6
+    {GPIOF, GPIO_PIN_7},  // DO7  = PF7
+    {GPIOF, GPIO_PIN_8},  // DO8  = PF8
+    {GPIOF, GPIO_PIN_9},  // DO9  = PF9
+    {GPIOF, GPIO_PIN_10}, // DO10 = PF10
+    
+    /* DO11-DO14: Port C (PC0-PC3) */
+    {GPIOC, GPIO_PIN_0},  // DO11 = PC0
+    {GPIOC, GPIO_PIN_1},  // DO12 = PC1
+    {GPIOC, GPIO_PIN_2},  // DO13 = PC2
+    {GPIOC, GPIO_PIN_3},  // DO14 = PC3
+    
+    /* DO15-DO22: Port A (PA0-PA7) */
+    {GPIOA, GPIO_PIN_0},  // DO15 = PA0
+    {GPIOA, GPIO_PIN_1},  // DO16 = PA1
+    {GPIOA, GPIO_PIN_2},  // DO17 = PA2
+    {GPIOA, GPIO_PIN_3},  // DO18 = PA3
+    {GPIOA, GPIO_PIN_4},  // DO19 = PA4
+    {GPIOA, GPIO_PIN_5},  // DO20 = PA5
+    {GPIOA, GPIO_PIN_6},  // DO21 = PA6
+    {GPIOA, GPIO_PIN_7},  // DO22 = PA7
+    
+    /* DO23-DO24: Port C (PC4-PC5) */
+    {GPIOC, GPIO_PIN_4},  // DO23 = PC4
+    {GPIOC, GPIO_PIN_5},  // DO24 = PC5
+    
+    /* DO25-DO27: Port B (PB0-PB2) */
+    {GPIOB, GPIO_PIN_0},  // DO25 = PB0
+    {GPIOB, GPIO_PIN_1},  // DO26 = PB1
+    {GPIOB, GPIO_PIN_2},  // DO27 = PB2
+    
+    /* DO28-DO29: Port F (PF11-PF12) */
+    {GPIOF, GPIO_PIN_11}, // DO28 = PF11
+    {GPIOF, GPIO_PIN_12}, // DO29 = PF12
+    
+    /* DO30: Port E (PE15) */
+    {GPIOE, GPIO_PIN_15}, // DO30 = PE15
+    
+    /* DO31-DO34: Port B (PB10-PB13) */
+    {GPIOB, GPIO_PIN_10}, // DO31 = PB10
+    {GPIOB, GPIO_PIN_11}, // DO32 = PB11
+    {GPIOB, GPIO_PIN_12}, // DO33 = PB12
+    {GPIOB, GPIO_PIN_13}, // DO34 = PB13
+    
+    /* DO35-DO42: Port D (PD8-PD15) */
+    {GPIOD, GPIO_PIN_8},  // DO35 = PD8
+    {GPIOD, GPIO_PIN_9},  // DO36 = PD9
+    {GPIOD, GPIO_PIN_10}, // DO37 = PD10
+    {GPIOD, GPIO_PIN_11}, // DO38 = PD11
+    {GPIOD, GPIO_PIN_12}, // DO39 = PD12
+    {GPIOD, GPIO_PIN_13}, // DO40 = PD13
+    {GPIOD, GPIO_PIN_14}, // DO41 = PD14
+    {GPIOD, GPIO_PIN_15}, // DO42 = PD15
+    
+    /* DO43-DO49: Port G (PG2-PG8) */
+    {GPIOG, GPIO_PIN_2},  // DO43 = PG2
+    {GPIOG, GPIO_PIN_3},  // DO44 = PG3
+    {GPIOG, GPIO_PIN_4},  // DO45 = PG4
+    {GPIOG, GPIO_PIN_5},  // DO46 = PG5
+    {GPIOG, GPIO_PIN_6},  // DO47 = PG6
+    {GPIOG, GPIO_PIN_7},  // DO48 = PG7
+    {GPIOG, GPIO_PIN_8},  // DO49 = PG8
+    
+    /* DO50-DO53: Port C (PC6-PC9) */
+    {GPIOC, GPIO_PIN_6},  // DO50 = PC6
+    {GPIOC, GPIO_PIN_7},  // DO51 = PC7
+    {GPIOC, GPIO_PIN_8},  // DO52 = PC8
+    {GPIOC, GPIO_PIN_9},  // DO53 = PC9
+    
+    /* DO54-DO55: Port A (PA8-PA9) */
+    {GPIOA, GPIO_PIN_8},  // DO54 = PA8
+    {GPIOA, GPIO_PIN_9}   // DO55 = PA9
 };
 
 #define NUM_OUTPUT_PINS (sizeof(outputPinMap) / sizeof(outputPinMap[0]))
